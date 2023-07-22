fcf8cfd664a319c6a2e51549cc979b31f216c69f
Alexgodwin113 <89025574+Alexgodwin113@users.noreply.github.com>
2023-07-22 13:26:41 +0100

    cd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
 
   const [counter, setCounter] = useState(0);
 
+  useEffect(() => {
+    setCounter(100);
+  }, []);
+
   return (
     <div className="App">
-      <button>-</button>
-      <h1>0</h1>
-      <button>+</button>
+      <button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div> 
   ); 
 }
